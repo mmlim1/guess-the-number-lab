2248d8a1347f8a3b4035c865fca6b902c09e7262
mmlim1 <michaelmlim1@gmail.com>
2022-05-30 13:47:40 -0700

    task 2

--- guess-the-number-lab/app.js
+++ guess-the-number-lab/app.js
@@ -1,15 +1,17 @@
 const game = {
+  prevGuesses: [],
   title: 'Guess the Number!',
   biggestNum: 100,
   smallestNum: 1,
   secretNum: null,
+  getGuess: prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}.`),
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
-  }
+    console.log(this.secretNum)
+  },
+  
 }
 
-//task 1 
-game['prevGuesses'] = []
-
 console.log(game)
+
