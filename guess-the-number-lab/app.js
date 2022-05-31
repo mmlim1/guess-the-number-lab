65ce12a25313d0d4c1a23f11110c1f5e1b26bc34
mmlim1 <michaelmlim1@gmail.com>
2022-05-30 17:40:08 -0700

    task 5-6

--- guess-the-number-lab/app.js
+++ guess-the-number-lab/app.js
@@ -5,29 +5,34 @@ const game = {
   smallestNum: 1,
   secretNum: null,
   play: function() {
-    let curGuess
     this.secretNum = Math.floor(Math.random() * 
     (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
     console.log(this.secretNum)
-    while (this.prevGuesses[this.prevGuesses.length-1] !==this.secretNum) {
+    while (this.prevGuesses[this.prevGuesses.length-1] !== this.secretNum) {
       this.getGuess()
-      this.prevGuesses.push(this.curGuess)
-    //if (this.prevGuesses[this.prevGuesses.length-1] > this.secrectNum) {
-
+      this.prevGuesses.push(this.getGuess)
+      console.log(this.prevGuesses)
+      this.render() 
+    } 
+  },
+  render: function() {
+    if (this.prevGuesses[this.prevGuesses.length-1] === this.secrectNum) {
+      alert(`Congrats you guessed the number in ${this.prevGuesses.length}!`)
+    } else if (this.prevGuesses[this.prevGuesses.length-1] > this.secrectNum) {
+      alert(`Your guess is too high Previous Guesses: ${this.prevGuesses}`)
+    } else if (this.prevGuesses[this.prevGuesses.length-1] < this.secrectNum) {
+      alert(`Your guess is too low Previous Guesses: ${this.prevGuesses}`)
     }
-  }
-},
+  },
   getGuess: function() {
     let curGuess
     while (!curGuess || curGuess < this.smallestNum || curGuess > this.biggestNum) {
       curGuess = parseInt(prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}.`))
     }
-    console.log(curGuess)
-    },
-
-  
+    return curGuess
+    
+  },
 }
-
 
 game.play()
 
